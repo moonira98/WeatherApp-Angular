c921727d1ce75151b6a7ecf1d067b11b1c46932a
Munira <mirzoevamunira98@gmail.com>
2025-11-11 22:59:00 +0500

    adaptiv

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { WeatherPanelComponent } from "../weather-panel/weather-panel.component";
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputIcon } from 'primeng/inputicon';
+import { IconField } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-home',
-  imports: [WeatherPanelComponent],
+  imports: [WeatherPanelComponent, IconFieldModule, InputIconModule, InputIcon, IconField, InputTextModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
